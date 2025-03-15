7f19f3ab3683e4f45c94ffb0edca7abf1fe8e960
DevMorningCoffee <132598665+DEVMorningCoffee@users.noreply.github.com>
2025-03-15 14:29:36 -0400

    Created the central bank and a commercial bank to allow communication between them

---
+++ src/main/java/com/personal_project/bank_application/Bank/CentralBank.java
@@ -0,0 +1,17 @@
+package com.personal_project.bank_application.Bank;
+
+public class CentralBank {
+    public String name;
+    public long ID;
+    public double balance;
+
+    public CentralBank() {};
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
---
+++ src/main/java/com/personal_project/bank_application/Bank/Commercial_Bank/JPMorgan.java
@@ -0,0 +1,12 @@
+package com.personal_project.bank_application.Bank.Commercial_Bank;
+
+
+import com.personal_project.bank_application.Bank.CentralBank;
+
+public class JPMorgan extends CentralBank {
+    public JPMorgan() {
+        this.name = "JPMorgan";
+        this.ID = 0;
+    }
+
+}
