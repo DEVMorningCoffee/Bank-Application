00970a6160594514d65e08fe1efb3b5f35582ce8
DevMorningCoffee <132598665+DEVMorningCoffee@users.noreply.github.com>
2025-03-17 22:33:58 -0400

    Create a new attribute and added a new DTO to parse json email requestbody

--- src/main/java/com/personal_project/bank_application/DTO/EmailDTO.java
+++ src/main/java/com/personal_project/bank_application/DTO/EmailDTO.java
@@ -1,18 +1,12 @@
 package com.personal_project.bank_application.DTO;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
-public class UserDTO {
-    private String firstName;
-    private String lastName;
-    private int bankID;
+public class EmailDTO {
     private String email;
 }
